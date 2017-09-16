ce0f3e9262be06fbe8cdfe90e93b326a751280bc
claudia <clou.7loymed@gmail.com>
2017-09-16 10:44:23 -0500

    color-selecionado

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+
+function red(){
+	document.getElementById("background").style.background="#ff0000";
+	document.getElementById("background").innerHTML="<b>RED</b>";
+}
+function blue(){
+	document.getElementById("background").style.background="#0101ff";
+	document.getElementById("background").innerHTML="<b>BLUE</b>"
+}
+function yellow(){
+	document.getElementById("background").style.background="#ffff00";
+	document.getElementById("background").innerHTML="<b>YELLOW</b>"
+}
+
+function newcolors(){
+	
+}
